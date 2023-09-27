255341810bc7f281274277ae0a0b9d0d9a2f17c7
LoremFooBar <loremfoobardev@gmail.com>
2023-09-27 19:35:04 +0300

    fix CA1815
    CA1815: Override equals and operator equals on value types

--- Emby.Naming/AudioBook/AudioBookNameParserResult.cs
+++ Emby.Naming/AudioBook/AudioBookNameParserResult.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Numerics;
+
 namespace Emby.Naming.AudioBook
 {
     /// <summary>
     /// Data object used to pass result of name and year parsing.
     /// </summary>
-    public struct AudioBookNameParserResult
+    public struct AudioBookNameParserResult : IEquatable<AudioBookNameParserResult>
     {
         /// <summary>
         /// Gets or sets name of audiobook.
@@ -14,5 +17,39 @@ namespace Emby.Naming.AudioBook
         /// Gets or sets optional year of release.
         /// </summary>
         public int? Year { get; set; }
+
+        /// <inheritdoc cref="IEqualityOperators{TSelf,TOther,TResult}.op_Equality(TSelf, TOther)" />
+        public static bool operator ==(AudioBookNameParserResult left, AudioBookNameParserResult right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <inheritdoc cref="IEqualityOperators{TSelf,TOther,TResult}.op_Inequality(TSelf, TOther)" />
+        public static bool operator !=(AudioBookNameParserResult left, AudioBookNameParserResult right)
+        {
+            return !(left == right);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object? obj)
+        {
+            if (obj is not AudioBookNameParserResult audioBookNameParserResult)
+            {
+                return false;
+            }
+
+            return Equals(audioBookNameParserResult);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode() => HashCode.Combine(Name, Year);
+
+        /// <summary>
+        /// Determine whether the specified object equals to the current object.
+        /// </summary>
+        /// <param name="other">the object to compare to.</param>
+        /// <returns>true if the the specified object is equal to the current object; otherwise false.</returns>
+        public bool Equals(AudioBookNameParserResult other) =>
+            Name.Equals(other.Name, StringComparison.Ordinal) && Nullable.Equals(Year, other.Year);
     }
 }
