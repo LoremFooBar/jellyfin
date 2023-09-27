abe442a27d1bb73e7fcafb14457094c983942fad
LoremFooBar <loremfoobardev@gmail.com>
2023-09-27 19:44:55 +0300

    fix CA1815
    CA1815: Override equals and operator equals on value types

--- Emby.Naming/Video/CleanDateTimeResult.cs
+++ Emby.Naming/Video/CleanDateTimeResult.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Numerics;
+
 namespace Emby.Naming.Video
 {
     /// <summary>
     /// Holder structure for name and year.
     /// </summary>
-    public readonly struct CleanDateTimeResult
+    public readonly struct CleanDateTimeResult : IEquatable<CleanDateTimeResult>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="CleanDateTimeResult"/> struct.
@@ -27,5 +30,40 @@ namespace Emby.Naming.Video
         /// </summary>
         /// <value>The year.</value>
         public int? Year { get; }
+
+        /// <inheritdoc cref="IEqualityOperators{TSelf,TOther,TResult}.op_Equality(TSelf, TOther)" />
+        public static bool operator ==(CleanDateTimeResult left, CleanDateTimeResult right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <inheritdoc cref="IEqualityOperators{TSelf,TOther,TResult}.op_Inequality(TSelf, TOther)" />
+        public static bool operator !=(CleanDateTimeResult left, CleanDateTimeResult right)
+        {
+            return !(left == right);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object? obj)
+        {
+            if (obj is not CleanDateTimeResult cleanDateTimeResult)
+            {
+                return false;
+            }
+
+            return Equals(cleanDateTimeResult);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode() => HashCode.Combine(Name, Year);
+
+        /// <summary>
+        /// Determine whether the specified object equals to the current object.
+        /// </summary>
+        /// <param name="other">the object to compare to.</param>
+        /// <returns>true if the the specified object is equal to the current object; otherwise false.</returns>
+        public bool Equals(CleanDateTimeResult other) =>
+            Name.Equals(other.Name, StringComparison.Ordinal)
+            && Nullable.Equals(Year, other.Year);
     }
 }
